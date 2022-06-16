f5b544860596c9c6819fad84f47c7348d357185a
jokeway <jokeway@gmail.com>
2022-06-16 15:55:16 +0800

    feat: initial packages

---
+++ macula-iam-application/src/main/java/org/macula/cloud/iam/IAMApplication.java
@@ -0,0 +1,13 @@
+package org.macula.cloud.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IAMApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(IAMApplication.class, args);
+	}
+
+}
